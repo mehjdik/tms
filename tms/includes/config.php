a32d3a7824ee334c712bfa2551ba712373d54c69
mehjdik <55868650+mehjdik@users.noreply.github.com>
2020-06-07 10:20:51 +0530

    Update config.php

--- tms/includes/config.php
+++ tms/includes/config.php
@@ -1,24 +1,16 @@
 <?php 
-// db cred for heroku
-private $hostname = "qf5dic2wzyjf1x5x.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-private $username = "q569t9v6ccpidqbe";
-private $password = "oauc52jqlah13p2u";
-private $db = "lm1wtcanixnoo3oz";
 // DB credentials.
-/*define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
-define('DB_NAME','tms');*/
+define('DB_HOST','qf5dic2wzyjf1x5x.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+define('DB_USER','q569t9v6ccpidqbe');
+define('DB_PASS','oauc52jqlah13p2u');
+define('DB_NAME','lm1wtcanixnoo3oz');
 // Establish database connection.
-
-$conn = mysqli_connect('localhost','root', '', 'new123');
-if(!$conn){
-        echo 'Please check your database connecton';
-    }
-//$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
-
-/*catch (PDOException $e)
+try
+{
+$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+}
+catch (PDOException $e)
 {
 exit("Error: " . $e->getMessage());
-}*/
+}
 ?>
