0e4b3f1392ccdc1640187f319ea372660eaa1548
mehjdik <55868650+mehjdik@users.noreply.github.com>
2020-06-07 10:15:01 +0530

    Update config.php

--- tms/includes/config.php
+++ tms/includes/config.php
@@ -1,4 +1,9 @@
 <?php 
+// db cred for heroku
+private $hostname = "qf5dic2wzyjf1x5x.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+private $username = "q569t9v6ccpidqbe";
+private $password = "oauc52jqlah13p2u";
+private $db = "lm1wtcanixnoo3oz";
 // DB credentials.
 define('DB_HOST','localhost');
 define('DB_USER','root');
@@ -7,7 +12,8 @@ define('DB_NAME','tms');
 // Establish database connection.
 try
 {
-$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+$conn = mysqli_connect('localhost','root', '', 'new123');
+//$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
 }
 catch (PDOException $e)
 {
