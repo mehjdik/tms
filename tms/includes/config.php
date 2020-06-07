5ac6e3d0c61c0249a16a5114b6376b68ac4a6bf5
mehjdik <55868650+mehjdik@users.noreply.github.com>
2020-06-07 10:16:24 +0530

    Update config.php

--- tms/includes/config.php
+++ tms/includes/config.php
@@ -5,18 +5,20 @@ private $username = "q569t9v6ccpidqbe";
 private $password = "oauc52jqlah13p2u";
 private $db = "lm1wtcanixnoo3oz";
 // DB credentials.
-define('DB_HOST','localhost');
+/*define('DB_HOST','localhost');
 define('DB_USER','root');
 define('DB_PASS','');
-define('DB_NAME','tms');
+define('DB_NAME','tms');*/
 // Establish database connection.
-try
-{
+
 $conn = mysqli_connect('localhost','root', '', 'new123');
+if(!$conn){
+        echo 'Please check your database connecton';
+    }
 //$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
-}
-catch (PDOException $e)
+
+/*catch (PDOException $e)
 {
 exit("Error: " . $e->getMessage());
-}
+}*/
 ?>
